330bbb41fdbdf2da15e179379dc632996b7f9d22
Soumojyoti Ganguly <soumojyotiganguly270@gmail.com>
2020-04-21 23:16:22 +0530

    Type coercion

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+/*
+var firstName='soumo';
+console.log(firstName);
+var  age = 20;
+var fullAge=true;
+
+console.log(fullAge);
+*/
+
+//Type coercion
+var name='soumo';
+var isTrue=true;
+console.log(name +' is programmer '+isTrue);
+
+//variable mutation
