1860679a0350aa7660dbc18bb4e9a6ea19f8ca56
Soumojyoti Ganguly <soumojyotiganguly270@gmail.com>
2020-04-24 23:40:08 +0530

    if else and imput from user

--- script.js
+++ script.js
@@ -7,9 +7,32 @@ var fullAge=true;
 console.log(fullAge);
 */
 
-//Type coercion
+//Type coercion ------------------------
 var name='soumo';
 var isTrue=true;
 console.log(name +' is programmer '+isTrue);
 
-//variable mutation
+//variable mutation and alert and prompt------------------------
+var age=28;
+ name = 'ujjwal';
+
+//var lastName=prompt('What is his last name: ');  //input from user
+//alert(name+' '+lastName);
+
+
+//typeof operator and logical operator--------------------------
+ var soumoAge=23;
+ var mistiAge= 22;
+ var ageCompare=soumoAge>mistiAge;
+ console.log(ageCompare);
+ console.log(typeof ageCompare);
+
+
+ //if else statement ---------------------------------------
+ var stName='soumo';
+ var stStatus='avg';
+ if(stStatus === 'good')
+   console.log(stName+' '+stStatus);
+ else {
+   console.log(stName+' '+stStatus);
+ }
