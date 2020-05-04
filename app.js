4470ae039c8bcf086c7de67b1f515ecdf006767b
soumojyotiganguly270@gmail.com <soumojyotiganguly270@gmail.com>
2020-05-04 08:52:52 +0530

    pic game

---
+++ app.js
@@ -0,0 +1,19 @@
+
+var score,roundScore,activePlayer;
+
+score=[0,0];
+roundScore=0;
+activePlayer=0;
+
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+    var dice=Math.floor(Math.random()*6)+1;
+    
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-'+dice+'.png';
+});
+
